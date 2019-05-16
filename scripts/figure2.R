8d4f75edc2b3d515c7a8d32ef3dea2cf1997d41e
afredstonhermann <alexa.fredstonhermann@gmail.com>
2019-05-15 18:34:12 -0700

    new figures

--- scripts/figure2.R
+++ scripts/figure2.R
@@ -2,15 +2,37 @@ library(here)
 library(tidyverse)
 library(purrr)
 library(broom)
+library(gridExtra)
 
 # load in datasets 
 poldat.stats.iso <- readRDS(here("processed-data","poldat.stats.iso.rds")) %>% 
   mutate(year = as.numeric(year)) 
 eqdat.stats.iso <- readRDS(here("processed-data","eqdat.stats.iso.rds"))%>% 
   mutate(year = as.numeric(year)) 
-btemp.stats <- readRDS(here("processed-data","soda_stats.rds")) 
-btemp.stats.overall <- btemp.stats %>% 
-  dplyr::select(btemp.year.mean, btemp.year.min, btemp.year.max, year_match) %>% 
+
+
+# use the .neus files not the .stats ones because we don't want to get confused with the adjusted years for the spring survey here--just want the actual years and temperatures
+hadisst.summary <- read_rds(here("processed-data","hadisst_neus.rds"))%>% 
+  group_by(year) %>% 
+  mutate(
+    year.month.mean = mean(sst),
+    year.month.sd = sd(sst),
+    year.month.max = max(sst),
+    year.month.min = min(sst)
+  ) %>% 
+  ungroup() %>% 
+  dplyr::select(year, year.month.mean, year.month.sd, year.month.max, year.month.min) %>% 
+  distinct()
+soda.summary <- read_rds(here("processed-data","soda_neus.rds")) %>% 
+  group_by(year) %>% 
+  mutate(
+    year.month.mean = mean(btemp),
+    year.month.sd = sd(btemp),
+    year.month.max = max(btemp),
+    year.month.min = min(btemp)
+  ) %>% 
+  ungroup() %>% 
+  dplyr::select(year, year.month.mean, year.month.sd, year.month.max, year.month.min) %>% 
   distinct()
 
 # run lms for plot 
@@ -25,6 +47,7 @@ poldat.lm <- poldat.stats.iso %>%
   ) %>% 
   unnest(tidymodel, .drop=TRUE) %>% 
   filter(term=="year")
+
 eqdat.lm <- eqdat.stats.iso %>% 
   dplyr::select(latinname, commonname, spp.dist05, year) %>% 
   distinct() %>% 
@@ -44,10 +67,10 @@ poldat.lm.gg <- poldat.lm %>%
          signif = ifelse(p.value<=0.05,"yes","no")) %>% 
   ggplot(aes(x=commonname, y=estimate, ymin=estimate-std.error, ymax=estimate+std.error, color=signif, fill=signif)) +
   geom_pointrange() +
-  scale_colour_manual(values=c('yes'='#0072B2', 'no'='#999999')) +
+  scale_colour_manual(values=c('yes'='darkorange', 'no'='#999999')) +
   geom_hline(yintercept=0, color="black") +
   labs(x=NULL, y="Poleward Edge Shift (km/yr)") +
-  scale_y_continuous(breaks=seq(-35, 35, 5), limits=c(-35, 35)) + 
+  scale_y_continuous(breaks=seq(-35, 20, 5), limits=c(-35, 20)) + 
   coord_flip() +
   theme_bw() +
   theme(legend.position = "none",
@@ -65,10 +88,10 @@ eqdat.lm.gg <- eqdat.lm %>%
          signif = ifelse(p.value<=0.05,"yes","no")) %>% 
   ggplot(aes(x=commonname, y=estimate, ymin=estimate-std.error, ymax=estimate+std.error, color=signif, fill=signif)) +
   geom_pointrange() +
-  scale_colour_manual(values=c('yes'='#0072B2', 'no'='#999999')) +
+  scale_colour_manual(values=c('yes'='darkorange', 'no'='#999999')) +
   geom_hline(yintercept=0, color="black") +
   labs(x=NULL, y="Equatorward Edge Shift (km/yr)") +
-  scale_y_continuous(breaks=seq(-35, 35, 5), limits=c(-35, 35)) + 
+  scale_y_continuous(breaks=seq(-35, 20, 5), limits=c(-35, 20)) + 
   coord_flip() +
   theme_bw() +
   theme(legend.position = "none",
@@ -82,24 +105,66 @@ eqdat.lm.gg <- eqdat.lm %>%
 
 # make assemblage edge plots 
 
-poldat.assemblage.gg <- poldat.stats.iso %>% 
-  filter(year >= min(btemp.stats.overall$year_match)) %>% 
-  left_join(btemp.stats.overall, by=c('year'='year_match')) %>% 
+temp.gg <- hadisst.summary %>% 
+  mutate(source = "SST") %>% 
+  full_join(soda.summary, by=c('year','year.month.mean','year.month.sd','year.month.max','year.month.min')) %>% 
+  mutate(source = replace_na(source, "SBT")) %>% 
   ggplot() +
-  geom_line(aes(x=year, y=btemp.year.mean*100),color="#0072B2",size=3) +
-  scale_y_continuous(sec.axis = sec_axis(~./100, name = "Mean Annual Sea Bottom Temperature (C)")) +
-  geom_line(aes(x=year, y=spp.dist95, group=commonname), color="#999999") +
-  geom_line(aes(x=year, y=assemblage.dist95), color="#D55E00", size=3) +  
-  scale_x_continuous(limits=c(min(btemp.stats.overall$year_match),max(btemp.stats.overall$year_match)), breaks=seq(min(btemp.stats.overall$year_match), max(btemp.stats.overall$year_match), 4)) +
-  labs(x="Year", y="Assemblage Edge Position (km)") +
+  geom_line(aes(x=year, y=year.month.mean, group=source, color=source),size=1.2) +
+  scale_y_continuous(limits=c(6, 16), breaks=seq(6, 16, 2)) +
+  scale_x_continuous(limits=c(min(hadisst.summary$year),max(hadisst.summary$year)), breaks=seq(min(hadisst.summary$year), max(hadisst.summary$year), 4)) +
+  scale_color_manual(values=c('#0072B2','navy')) +
+  labs(x="Year", y="Mean Annual Temperature (C)") +
   theme_bw() +
-  theme(legend.position = "none",
+  theme(legend.title=element_blank(), 
+        legend.position=c(.1, .2),
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(),
         text=element_text(family="sans",size=12,color="black"),
         legend.text = element_text(size=12),
         axis.title=element_text(family="sans",size=12,color="black"),
+        axis.text.x = element_text(angle = 90, hjust = 1), 
         axis.text=element_text(family="sans",size=8,color="black")) +
   NULL
-  
 
+pol.assemb.gg <- poldat.stats.iso %>% 
+  ggplot(aes(x=year, y=assemblage.dist95)) +
+  geom_line(size=1.2, color="darkorange") +
+  scale_y_continuous(limits=c(400, 1600), breaks=seq(400, 1600, 200)) +
+  scale_x_continuous(limits=c(min(poldat.stats.iso$year),max(poldat.stats.iso$year)), breaks=seq(min(poldat.stats.iso$year), max(poldat.stats.iso$year), 4)) +
+  labs(title="Poleward Assemblage",x="Year", y="Edge Position (km)") +
+  theme_bw() +
+  theme(legend.position="none",
+        plot.title = element_text(margin = margin(t = 10, b = -20)), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        text=element_text(family="sans",size=12,color="black"),
+        legend.text = element_text(size=12),
+        axis.title=element_text(family="sans",size=12,color="black"),
+        axis.text.x = element_text(angle = 90, hjust = 1), 
+        axis.text=element_text(family="sans",size=8,color="black")) +
+  NULL
+
+eq.assemb.gg <- eqdat.stats.iso %>% 
+  ggplot(aes(x=year, y=assemblage.dist05)) +
+  geom_line(size=1.2, color="darkorange") +
+  scale_y_continuous(limits=c(400, 1600), breaks=seq(400, 1600, 200)) +
+  scale_x_continuous(limits=c(min(eqdat.stats.iso$year),max(eqdat.stats.iso$year)), breaks=seq(min(eqdat.stats.iso$year), max(eqdat.stats.iso$year), 4)) +
+  labs(title="Equatorward Assemblage",x="Year", y="Edge Position (km)") +
+  theme_bw() +
+  theme(legend.position="none",
+        plot.title = element_text(margin = margin(t = 10, b = -20)), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        text=element_text(family="sans",size=12,color="black"),
+        legend.text = element_text(size=12),
+        axis.title=element_text(family="sans",size=12,color="black"),
+        axis.text.x = element_text(angle = 90, hjust = 1), 
+        axis.text=element_text(family="sans",size=8,color="black")) +
+  NULL
+
+# couldn't get grid.arrange or cowplot to arrange these correctly--keep working on it! 
+fig1A <- grid.arrange(temp.gg, pol.assemb.gg, eq.assemb.gg, ncol=1)
+ggsave(fig1A, filename=here("results","fig1_part1.png"),height=8, width=4, dpi=300, scale=1.1)
+fig1B <- grid.arrange(poldat.lm.gg, eqdat.lm.gg, ncol=2)
+ggsave(fig1B, filename=here("results","fig1_part2.png"),height=8, width=7, dpi=300, scale=0.9)
---
+++ scripts/figure3.R
@@ -0,0 +1,41 @@
+library(tidyverse)
+library(here)
+library(ggplot2)
+library(directlabels) 
+
+poldat.stats <- readRDS(here("processed-data","poldat.stats.rds"))
+eqdat.stats <- readRDS(here("processed-data","eqdat.stats.rds"))
+
+pol.ex <- poldat.stats %>% 
+  filter(commonname=="Blueback shad") %>% 
+  dplyr::select(year, spp.dist95, commonname) %>% 
+  distinct() %>% 
+  rename(edge.dist = spp.dist95)
+
+eq.ex <- eqdat.stats %>% 
+  filter(commonname=="White hake")%>% 
+  dplyr::select(year, spp.dist05, commonname) %>% 
+  distinct()%>% 
+  rename(edge.dist = spp.dist05)
+
+fig3 <- rbind(pol.ex, eq.ex) %>% 
+  ggplot(aes(x=year, y=edge.dist, group=commonname, color = commonname)) + 
+  geom_line() + 
+  geom_point(size=0.75) + 
+  scale_color_manual(values=c('darkorange','orangered')) +
+  labs(x="Year", y="Edge Position (km)") +
+  scale_y_continuous(limits=c(400, 1600), breaks=seq(400, 1600, 200)) +
+  scale_x_continuous(limits=c(min(eqdat.stats$year),max(eqdat.stats$year)), breaks=seq(min(eqdat.stats$year), max(eqdat.stats$year), 4)) +
+  theme_bw() +
+  theme(legend.title=element_blank(), 
+        legend.position=c(.8, .12),,
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        text=element_text(family="sans",size=12,color="black"),
+        legend.text = element_text(size=12),
+        axis.title=element_text(family="sans",size=12,color="black"),
+        axis.text.x = element_text(angle = 90, hjust = 1), 
+        axis.text=element_text(family="sans",size=8,color="black")) +
+  NULL
+
+ggsave(fig3, filename=here("results","figure3.png"),width=8,height=5, scale=0.9, dpi=300)
